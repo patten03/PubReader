02b5340847dd0358d4be1e870b39d952f300c68b
Patte <patten010203@gmail.com>
2024-03-02 13:37:44 +0300

    deleted unused findFolder()

--- DirWork.h
+++ DirWork.h
@@ -16,11 +16,8 @@ std::string findFile(std::string title, const fileType& type);
 std::string findFolder(std::string title);
 
 //составные части findFile() и findFolder()
-bool dirAccess(const std::string& folder);
 std::string askName();
 int movingArrow(int ymin, int ymax, int cur, int key);
-void makeDirList(std::string folder, std::vector<std::string>& folderList);
-void folderChoice(int menuSize, std::vector<std::string> folders, int& cur, bool& agree, std::string& folder);
 void fileChoice(int begCoord, std::vector<std::string> folders, int& cur, std::string& filepath);
 void makeFilesList(std::string filepath, std::vector<std::string>& folderList, fileType type);
 
